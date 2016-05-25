0ebf0e47f4246700b01dc80361ce23839ac39c36
airtoxin <airtoxin@icloud.com>
2016-05-26 08:24:37 +0900

    Change video

--- src/App/index.js
+++ src/App/index.js
@@ -46,7 +46,7 @@ export default class App extends React.Component {
             <div style={styles.app}>
                 <div style={styles.overlay}/>
                 <HeaderNav style={styles.headerNav}/>
-                <YouTube videoId="Ftm2uv7-Ybw" opts={opts}/>
+                <YouTube videoId="IvjMgVS6kng" opts={opts}/>
                 <FooterNav />
             </div>
         );
